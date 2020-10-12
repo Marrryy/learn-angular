1a013c152f55c4c93b7858b57799d9aba98151d0
= <=>
2020-10-12 14:23:39 +0700

    Add Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 import { Hero } from '../hero';
-// import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service'; 
+import { MessagesService } from "../messages.service";
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +11,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name :'Windstorm'
-  // };
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero : Hero;
+
   onSelect(hero : Hero):void{
     this.selectedHero= hero;
+    this.messagesService.add(`Heroes Component: Selected hero id=${hero.id}`);
   }
 
-  constructor() { 
+  getHeroes():void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService:HeroService, private messagesService:MessagesService) { 
   }
 
   ngOnInit() {
-    
+    this.getHeroes()
   }
 
 }
